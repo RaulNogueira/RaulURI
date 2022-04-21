15b01755dd17986e6c5c2f2950ea652f0a78baf8
Raul Nogueira Neves <rnneves@stefanini.com>
2022-04-21 13:18:39 -0300

    Novos Arquivos

--- src/Iniciante/URI1079.java
+++ src/Iniciante/URI1079.java
@@ -2,7 +2,7 @@ package Iniciante;
 
 import java.util.Scanner;
 
-public class URI1071 {
+public class URI1079 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
---
+++ src/Iniciante/URI1095.java
@@ -0,0 +1,14 @@
+package Iniciante;
+
+public class URI1095 {
+
+	public static void main(String[] args) {
+		int i=1, j=60;
+		while (j >= 0) {
+			System.out.println("I=" + i + " J=" + j);
+			j-=5;
+			i+=3;
+		}
+	}
+
+}
